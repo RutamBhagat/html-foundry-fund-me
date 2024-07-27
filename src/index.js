9d03cba4eb31a40b82b7c1da3a446a43b7aabfe2
RutamBhagat <rutambhagat@gmail.com>
2024-07-27 19:58:05 +0530

    fix breaking changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { ethers } from "./ethers-6.7.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
+
+import { ethers } from "./ethers-6.7.esm.min.js"
 
 const connectButton = document.getElementById("connectButton")
 const withdrawButton = document.getElementById("withdrawButton")
@@ -13,13 +14,53 @@ balanceButton.onclick = getBalance
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
+      // Request account access
       await ethereum.request({ method: "eth_requestAccounts" })
+
+      // Check if we're on the correct network (Sepolia)
+      const chainId = await ethereum.request({ method: "eth_chainId" })
+      if (chainId !== "0xaa36a7") {
+        // Sepolia's chain ID
+        try {
+          // Try to switch to Sepolia
+          await ethereum.request({
+            method: "wallet_switchEthereumChain",
+            params: [{ chainId: "0xaa36a7" }], // Sepolia's chain ID
+          })
+        } catch (switchError) {
+          // This error code indicates that the chain has not been added to MetaMask
+          if (switchError.code === 4902) {
+            try {
+              await ethereum.request({
+                method: "wallet_addEthereumChain",
+                params: [
+                  {
+                    chainId: "0xaa36a7",
+                    chainName: "Sepolia Testnet",
+                    nativeCurrency: {
+                      name: "Sepolia Ether",
+                      symbol: "SEP",
+                      decimals: 18,
+                    },
+                    rpcUrls: ["https://rpc.sepolia.org"],
+                    blockExplorerUrls: ["https://sepolia.etherscan.io"],
+                  },
+                ],
+              })
+            } catch (addError) {
+              console.error("Failed to add Sepolia network", addError)
+            }
+          }
+          console.error("Failed to switch to Sepolia network", switchError)
+        }
+      }
+
+      connectButton.innerHTML = "Connected"
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+      console.log(accounts)
     } catch (error) {
       console.log(error)
     }
-    connectButton.innerHTML = "Connected"
-    const accounts = await ethereum.request({ method: "eth_accounts" })
-    console.log(accounts)
   } else {
     connectButton.innerHTML = "Please install MetaMask"
   }
@@ -29,7 +70,7 @@ async function withdraw() {
   console.log(`Withdrawing...`)
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.BrowserProvider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
+    await provider.send("eth_requestAccounts", [])
     const signer = await provider.getSigner()
     const contract = new ethers.Contract(contractAddress, abi, signer)
     try {
@@ -50,13 +91,17 @@ async function fund() {
   console.log(`Funding with ${ethAmount}...`)
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.BrowserProvider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
+    await provider.send("eth_requestAccounts", [])
     const signer = await provider.getSigner()
     const contract = new ethers.Contract(contractAddress, abi, signer)
     try {
-      const transactionResponse = await contract.fund(2, "0x0000000000000000000000000000000000000000", {
-        value: ethers.utils.parseEther(ethAmount),
-      })
+      const transactionResponse = await contract.fund(
+        2,
+        "0x0000000000000000000000000000000000000000",
+        {
+          value: ethers.parseEther(ethAmount),
+        }
+      )
       await transactionResponse.wait(1)
     } catch (error) {
       console.log(error)
@@ -68,10 +113,10 @@ async function fund() {
 
 async function getBalance() {
   if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.BrowserProvider(window.ethereum)
     try {
       const balance = await provider.getBalance(contractAddress)
-      console.log(ethers.utils.formatEther(balance))
+      console.log(ethers.formatEther(balance))
     } catch (error) {
       console.log(error)
     }
